eeea83f1c06709bfbac11307f2f8f92f9385e25e
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-10-26 17:06:10 +0200

    add tools and footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -57,12 +57,14 @@
     </header>
 
 
+    <!-- jumbotron -->
     <section class="jumbotron">
         <div class="my_container py-5">
         </div>
     </section>
 
 
+    <!-- comics section -->
     <div class="comics_section">
         <!-- cards row -->
         <div class="row pt-5 my_container">
@@ -96,8 +98,201 @@
                 LOAD MORE
             </button>
         </div>
-
     </div>
+
+    <!-- DC Tools -->
+    <section class="py-5 tools">
+        <div class="wrapper d-flex tools_container justify-content-between align-items-center">
+            <div class="col-2">
+                <a href="#">
+                    <img src="{{Vite::asset('resources/images/buy-comics-digital-comics.png')}}" alt="">
+                    <span>DIGITAL COMICS</span>
+                </a>
+            </div>
+            <div class="col-2">
+                <a href="#">
+                    <img src="{{Vite::asset('resources/images/buy-comics-merchandise.png')}}" alt="" srcset="">
+                    <span>DC MERCHANDISE</span>
+                </a>
+            </div>
+            <div class="col-2">
+                <a href="#">
+                    <img src="{{Vite::asset('resources/images/buy-comics-subscriptions.png')}}" alt="" srcset="">
+                    <span>SUBSCRIPTION</span>
+                </a>
+            </div>
+            <div class="col-2">
+                <a href="#">
+                    <img src="{{Vite::asset('resources/images/buy-comics-shop-locator.png')}}" alt="" srcset="" class="comic_shop">
+                    <span>COMIC SHOP LOCATION</span>
+                </a>
+            </div>
+            <div class="col-2">
+                <a href="#">
+                    <img src="{{Vite::asset('resources/images/buy-comics-digital-comics.png')}}" srcset="">
+                    <span>DC POWER VISA</span>
+                </a>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer -->
+    <footer>
+        <!-- main footer -->
+        <section class="main_footer">
+            <div class="row my_container flex-nowrap">
+                <!-- columns on left with all links -->
+                <div class="col-5 py-4">
+                    <div class="row py-4">
+                        <!-- dc comics and shop col wrapper -->
+                        <div class="col-4">
+                            <!-- DC comics section -->
+                            <div class="col-12">
+                                <h6>
+                                    DC COMICS
+                                </h6>
+                                <div class="d-flex flex-column">
+                                    <a href="#">
+                                        Characters
+                                    </a>
+                                    <a href="#">
+                                        Comics
+                                    </a>
+                                    <a href="#">
+                                        Movies
+                                    </a>
+                                    <a href="#">
+                                        TV
+                                    </a>
+                                    <a href="#">
+                                        Games
+                                    </a>
+                                    <a href="#">
+                                        Videos
+                                    </a>
+                                    <a href="#">
+                                        Games
+                                    </a>
+                                </div>
+                            </div>
+                            <!-- Shop section -->
+                            <div class="col-12 pt-4">
+                                <h6>
+                                    SHOP
+                                </h6>
+                                <div class="d-flex flex-column">
+                                    <a href="#">
+                                        Shop DC
+                                    </a>
+                                    <a href="#">
+                                        Shop DC Collectibles
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Dc section -->
+                        <div class="col-4">
+                            <h6>
+                                DC
+                            </h6>
+                            <div class="d-flex flex-column">
+                                <a href="#">
+                                    Terms of use
+                                </a>
+                                <a href="#">
+                                    Privacy policy (New)
+                                </a>
+                                <a href="#">
+                                    Ad Choices
+                                </a>
+                                <a href="#">
+                                    Advertising
+                                </a>
+                                <a href="#">
+                                    Jobs
+                                </a>
+                                <a href="#">
+                                    Subscriptions
+                                </a>
+                                <a href="#">
+                                    Talent Workshop
+                                </a>
+                                <a href="#">
+                                    CPSC Certificates
+                                </a>
+                                <a href="#">
+                                    Ratings
+                                </a>
+                                <a href="#">
+                                    Shop Help
+                                </a>
+                                <a href="#">
+                                    Contact Us
+                                </a>
+                            </div>
+                        </div>
+                        <!-- Sites section -->
+                        <div class="col-4">
+                            <h6>
+                                SITES
+                            </h6>
+                            <div class="d-flex flex-column">
+                                <a href="#">
+                                    DC
+                                </a>
+                                <a href="#">
+                                    MAD Magazine
+                                </a>
+                                <a href="#">
+                                    DC Kids
+                                </a>
+                                <a href="#">
+                                    DC Universe
+                                </a>
+                                <a href="#">
+                                    DC Power Vista
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- right side with logo bg -->
+                <div class=".col-7 logo d-flex flex-shrink-1 align-self-center justify-content-center">
+                </div>
+            </div>
+        </section>
+        <!-- bottom footer -->
+        <section class="bottom_footer">
+            <div class="row my_container py-5 align-items-center justify-content-between">
+                <!-- col with sign up button -->
+                <div class="col-6">
+                    <button type="button" class="btn my_border text-light P-2">SIGN UP NOW</button>
+                </div>
+                <!-- col with follow us and medias -->
+                <div class="col-4 d-flex align-items-center justify-content-between">
+                    <p class="follow">FOLLOW US</p>
+                    <!-- wrapper with medias -->
+                    <div class="media_wrapper text-dark pb-3">
+                        <a href="#">
+                            <img src="../assets/img/footer-facebook.png" alt="" srcset="">
+                        </a>
+                        <a href="#">
+                            <img src="../assets/img/footer-twitter.png" alt="" srcset="">
+                        </a>
+                        <a href="#">
+                            <img src="../assets/img/footer-youtube.png" alt="">
+                        </a>
+                        <a href="#">
+                            <img src="../assets/img/footer-pinterest.png" alt="" srcset="">
+                        </a>
+                        <a href="#">
+                            <img src="../assets/img/footer-periscope.png" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </footer>
 
 
 
